1d05dd078a4e8f89f30dcd7f236b6bb13a2a23cf
layesh1 <layesh1@charlotte.edu>
2025-11-30 19:41:18 -0500

    Your descriptive commit message

--- dashboard/pages/app.py
+++ dashboard/pages/app.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+if ROOT not in sys.path:
+    sys.path.append(ROOT)
+ 
+
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
